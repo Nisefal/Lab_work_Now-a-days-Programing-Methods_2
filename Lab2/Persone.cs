d8d17e1096ae4da5012bb4d17391056e7d0cf6a2
Nisefal <kolyabogdanenko@gmail.com>
2017-12-20 00:25:00 +0200

    -

---
+++ Lab2/Persone.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Card
+{
+    class Persone
+    {
+        public string Name_SName { get; private set; }
+        public string PC { get; private set; }
+
+        internal Lab2.MyFacade MyFacade
+        {
+            get => default(Lab2.MyFacade);
+            set
+            {
+            }
+        }
+
+        public Persone(string name, string code)
+        {
+            Name_SName = name;
+            PC = code;
+        }
+    }
+}
